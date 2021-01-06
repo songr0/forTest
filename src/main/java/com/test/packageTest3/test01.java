9dd97172bf8f692a703f0e0f2d08e4a89a1d65f1
hzy <wy5030620@163.com>
2021-01-06 10:27:18 +0800

    类新增注解

--- src/main/java/com/test/packageTest3/test01.java
+++ src/main/java/com/test/packageTest3/test01.java
@@ -1,7 +1,21 @@
 package com.test.packageTest3;
 
+import org.springframework.stereotype.Component;
+
+@Component("c")
 public class test01 {
 
+    private int number;
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Deprecated
     public class test01Inner {
         public void method01() {
             System.out.println("innerMethod");
@@ -18,13 +32,15 @@ public class test01 {
     }
 
     public static void main(String[] args) {
+        test01 test = new test01();
+        test.setNumber(1);
+        System.out.println(test.getNumber());
         System.out.println("test01");
         test01 object = new test01();
         test01Inner obj = object.new test01Inner();
         obj.method01();
         object.method02();
         object.method03();
-
     }
 
 }
